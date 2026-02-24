1aeb979db473cf235d7b2db0e69892c2447186bd
Lobaum <zpedrohenry@gmail.com>
2026-02-24 15:05:46 -0300

    jogador_inimigo

---
+++ rpg_engine/inimigo.py
@@ -0,0 +1,9 @@
+class Inimigo:
+    def __init__(self, poder, defesa, vida_maxima, esquiva, exp_recompensa, dificuldade):
+        self.poder = poder
+        self.defesa = defesa
+        self.vida_maxima = vida_maxima
+        self.vida_atual = vida_maxima
+        self.esquiva = esquiva
+        self.exp_recompensa = exp_recompensa
+        self.dificuldade = dificuldade
---
+++ rpg_engine/jogador.py
@@ -0,0 +1,12 @@
+class Jogador:
+    def __init__(self, poder, defesa, vida_maxima, esquiva, raca, vocacao):
+        self.poder = poder
+        self.defesa = defesa
+        self.vida_maxima = vida_maxima
+        self.vida_atual = vida_maxima
+        self.esquiva = esquiva 
+        self.nivel = 1
+        self.exp = 0
+        self.raca = raca
+        self.vocacao = vocacao
+        self.inventario = []
