a3e92d95c22aecd5660f2927f84989ada9a750f8
Lobaum <zpedrohenry@gmail.com>
2026-02-25 16:15:09 -0300

    arrumandoinimigo

--- entidades/inimigo.py
+++ entidades/inimigo.py
@@ -2,10 +2,6 @@ from entidades import Entidade
 
 class Inimigo:
     def __init__(self, poder, defesa, vida_maxima, esquiva, exp_recompensa, dificuldade):
-        self.poder = poder
-        self.defesa = defesa
-        self.vida_maxima = vida_maxima
-        self.vida_atual = vida_maxima
-        self.esquiva = esquiva
+        super().__init__(poder, defesa, vida_maxima, esquiva)
         self.exp_recompensa = exp_recompensa
         self.dificuldade = dificuldade
