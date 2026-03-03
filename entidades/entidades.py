cc12c63357ff9474d2660a88e0276645c74b007e
Lobaum <zpedrohenry@gmail.com>
2026-03-03 15:53:54 -0300

    funcaoatacar

--- entidades/entidades.py
+++ entidades/entidades.py
@@ -1,3 +1,5 @@
+import random
+
 class Entidade:
     def __init__(self, poder, defesa, vida_maxima:int, esquiva):
         self.poder = poder
@@ -15,5 +17,38 @@ class Entidade:
         else:
             print(f"morto: {self.vida_atual}/{self.vida_maxima}")
 
-vivo = Entidade(10, 5, 18, 0.2)
-vivo.status()
+    def atacar(self, alvo):
+        d20 = random.randint(1, 20)
+        print(f"D20 = {d20}")
+        if d20 == 1:
+            print(f"Rolou {d20}! Ataque Falhou")
+        elif d20 + self.poder >= alvo.defesa + 10:
+            print(f"Ataque quebrou a defesa")
+            d6 = random.randint(1, 6)
+
+            if d20 == 20:
+                print(f"Rolou um {d20}!! Acerto CRÍTICO!!")
+                dano = self.poder + d6
+                dano_final = dano * 2
+
+            else:
+                dano = (self.poder + d6) - alvo.defesa
+                dano_final = max(1, dano)
+
+            alvo.receber_dano(dano_final)
+            print(f"O alvo recebeu {dano_final} de dano! (Vida restante: {alvo.vida_atual})")
+
+        else:
+            print(f"Defesa do alvo muito grande")
+
+    def receber_dano(self, dano):
+        self.vida_atual = self.vida_atual - dano
+        self.vida_atual = max(0, self.vida_atual)
+        
+
+
+MC = Entidade(15, 10, 30, 2)
+
+Inimigo = Entidade(12, 5, 25, 2)
+
+MC.atacar(Inimigo)
