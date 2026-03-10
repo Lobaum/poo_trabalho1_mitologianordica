48ba5b4e404dc9692fcf84773e3934861fd7d0fd
Lobaum <zpedrohenry@gmail.com>
2026-03-10 13:37:30 -0300

    testecombatenovo

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from entidades.jogador import Jogador
-from entidades.inimigo import Inimigo, inimigos
+from entidades.inimigo import spawn_inimigo
 from combate.combate import iniciar_combate
 
-player = Jogador(poder=10, defesa=10, vida_maxima=10, esquiva=2, raca="Humano", vocacao="Guerreiro", energia=10, pocao=3, exp=0)
-dados_lobo = inimigos['Fácil']
-lobo = Inimigo(
-    poder=dados_lobo['Poder'], 
-    defesa=dados_lobo['Defesa'], 
-    vida_maxima=dados_lobo['Vida'], 
-    esquiva=dados_lobo['Esquiva'], 
-    energia=dados_lobo['Energia'], 
-    exp_recompensa=50, 
-    dificuldade="Fácil"
-)
+player = Jogador(poder=100, defesa=10, vida_maxima=10, esquiva=2, raca="Humano", vocacao="Guerreiro", energia=10, pocao=3, exp=0)
 
-iniciar_combate(player, lobo)
+adversario = spawn_inimigo(16)
+
+iniciar_combate(player, adversario)
